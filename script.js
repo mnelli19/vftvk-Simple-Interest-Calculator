6e39280be43a886547ef497a59961eec8be62a43
Mario <nllmra19@gmail.com>
2021-09-29 23:12:41 +0000

    lab mario

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
+
 
 function updateRate() 
 {
@@ -12,7 +8,11 @@ function updateRate()
 
 function compute()
 {
-
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
     
 }
