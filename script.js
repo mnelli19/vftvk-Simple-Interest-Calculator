3d6a8a325be521d62c307f51ada9f20f8bb47423
Mario <nllmra19@gmail.com>
2021-09-29 23:09:01 +0000

    lab mario

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-function compute()
-{
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    p = document.getElementById("principal").value;
-
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-    
-}
-
-
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+
+function compute()
+{
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    
+}
+
+
+
 
 // validation for "Principal" input box. If the user enters zero or negative values, show an alert "Enter a positive number"
 // nce the user clicks on the alert "OK" button, take the user back to the "Principal" input box, by setting the focus on this box.
